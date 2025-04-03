38eb0c858c6c0d32656923955322d1667a9974a3
sudo97 <i.mihnevich@gmail.com>
2025-04-03 19:54:46 +0300

    Enhance Tetris game implementation by adding core game logic for rotation and movement. Update test suite to include comprehensive tests for Tetris game mechanics, specifically for the step and rotate functions. Modify cabal file to include containers as a dependency for the new functionality.

--- src/Game/Tetris/Core.hs
+++ src/Game/Tetris/Core.hs
@@ -1,1 +1,22 @@
-module Game.Tetris.Core where
+module Game.Tetris.Core (TetrisGame (..), step, rotate) where
+
+import qualified Data.Set as Set
+
+data TetrisGame = TetrisGame {}
+
+type Point = (Int, Int)
+
+step :: Set.Set Point -> Set.Set Point
+step = Set.map (\(x, y) -> (x, y - 1))
+
+topLeft :: Set.Set Point -> Point
+topLeft pts = (minimum (Set.map fst pts), maximum (Set.map snd pts))
+
+rotate :: Set.Set Point -> Set.Set Point
+rotate pts =
+  let (rotX, rotY) = topLeft pts
+      newFigure = Set.map (\(x, y) -> (rotX + y - rotY, rotY - x + rotX)) pts
+      (topX, topY) = topLeft newFigure
+      deltaX = rotX - topX
+      deltaY = rotY - topY
+   in Set.map (\(x, y) -> (x + deltaX, y + deltaY)) newFigure
--- test/TestTetris.hs
+++ test/TestTetris.hs
@@ -1,13 +1,34 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module TestTetris (testTetris) where
 
+import qualified Data.Set as Set
+import Game.Tetris.Core (rotate, step)
 import Test.HUnit
 
 testTetris :: Test
-testTetris =
-  TestLabel
-    "Tetris"
-    ( TestList
-        [ TestCase $ do
-            assertFailure "Not implemented"
-        ]
-    )
+testTetris = TestLabel "Tetris" (TestList [testStep, testRotate])
+
+testRotate :: Test
+testRotate =
+  TestLabel "rotate" $
+    TestList
+      [ TestCase $ do
+          let dot = Set.fromList [(5, 3)]
+          let expected = Set.fromList [(5, 3)]
+          assertEqual "Dot should rotate" expected (rotate dot)
+          let twoPts = Set.fromList [(5, 3), (6, 3)]
+          let expected = Set.fromList [(5, 3), (5, 2)]
+          assertEqual "Two points should rotate" expected (rotate twoPts)
+          let straight = Set.fromList [(5, 3), (5, 2), (5, 1)]
+          let expected = Set.fromList [(5, 3), (6, 3), (7, 3)]
+          assertEqual "Straight should rotate" expected (rotate straight)
+          assertEqual "Straight should rotate 4 times" straight (rotate (rotate (rotate (rotate straight))))
+      ]
+
+testStep :: Test
+testStep = TestLabel "step" $
+  TestCase $ do
+    let squareShape = Set.fromList [(0, 10), (0, 9), (1, 10), (1, 9)]
+    let expected = Set.fromList [(0, 9), (0, 8), (1, 9), (1, 8)]
+    assertEqual "Square shape should move down" expected (step squareShape)
